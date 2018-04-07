4cfb516820fe580ce1920b7bcaa3a14eee3973ae
lucas009009 <lucas009009@gmail.com>
2018-04-07 08:30:48 +0200

    JMX

--- fr.epsi.myEpsi/src/fr/epsi/myEpsi/jmx/Premier.java
+++ fr.epsi.myEpsi/src/fr/epsi/myEpsi/jmx/Premier.java
@@ -1,5 +1,33 @@
 package fr.epsi.myEpsi.jmx;
 
-public class Premier {
+import fr.epsi.myEpsi.dao.IAnnonceDao;
+import fr.epsi.myEpsi.dao.mockImpl.AnnonceDao;
 
+public class Premier implements PremierMBean {
+
+    private static String nom = "PremierMBean";
+    private int valeur = 100;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getValeur() {
+        return valeur;
+    }
+
+    public synchronized void setValeur(int valeur) {
+        this.valeur = valeur;
+    }
+
+    public void nbAnnonce() {
+        System.out.println("Compte le nombre d'annonce");
+        IAnnonceDao annonceDao = new AnnonceDao();
+        int nbAnnonce = annonceDao.countAnnonces();
+
+    }
+
+    public Premier() {
+
+    }
 }
--- fr.epsi.myEpsi/src/fr/epsi/myEpsi/jmx/PremierMBean.java
+++ fr.epsi.myEpsi/src/fr/epsi/myEpsi/jmx/PremierMBean.java
@@ -1,5 +1,14 @@
 package fr.epsi.myEpsi.jmx;
 
+import java.io.IOException;
+
 public interface PremierMBean {
+
+    public String getNom() throws IOException;
+
+    public int getValeur() throws IOException;
+    public void setValeur(int valeur) throws IOException;
+
+    public void nbAnnonce() throws IOException;
 
 }
